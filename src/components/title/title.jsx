98dbd51120c5a82de025a67fbdd4be82f7acedc2
gheezl <gheezl25@gmail.com>
2020-09-08 11:24:09 -0400

    Added the title component

---
+++ src/components/title/title.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react"
+import "./title.css"
+
+
+const Title = () => {
+    return (
+        <Fragment>
+            <div className="title-border" >
+                <span className="name" >Jacob Rushlow</span>
+                <span className="developer-designer" >A Web Developer and Designer</span>
+            </div>
+        </Fragment>
+    )
+}
+
+
+
+export default Title;
