0811ead5601ee9c06fc70e4251b4a029967a4fb1
gheezl <gheezl25@gmail.com>
2020-09-09 20:21:28 -0400

    Removed some things from the about me page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,15 +12,6 @@ const About = () => {
                     <img className="image" src={Me} />
                     <span className="description">Hi! My name is Jacob Rushlow I am a freelance web developer and web designer currently living in northwestern Ohio.</span>
                 </div>
-                {/* <span className="title" >What I do</span>
-                <div className="skills">
-                    <span className="web-development">Web Development</span>
-                    <span className="web-design" >Web Design</span>
-                    <span className="custom-projects"  >Custom Projects</span>
-                </div> */}
-                <div className="title" >
-                    <span>Testimonials</span>
-                </div>
             </div>
         </Fragment>
     )
